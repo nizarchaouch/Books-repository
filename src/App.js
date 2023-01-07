e156f7ec66ebf96e72360a80fefb6c8acd368388
Chaouch Nizar <chaouchnizar1@gmail.com>
2023-01-07 09:19:40 +0100

    clear code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import './App.css';
 import './Components/Card.css';
-import FindBook from './Components/FindBook';
-import NewBook from './Components/NewBook';
-import Card from './Components/Card';
-import { useState } from 'react';
 import Main from './Components/Main';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import About from './Page/About';
 import Home from './Page/Home';
 
 function App() {
-  /* const [books, setbooks] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isbtn, setbtn] = useState(true);
-  const getbooks = async () => {
-    setIsLoading(true);
-    try {
-      const response = await fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyCgTzxNyBTZLVvTtCBK6joWCxHaSy9gZQo" + '&maxResults=21');
-      if (!response.ok) {
-        throw new Error("Something went wrong!!!!");
-      }
-      const data = await response.json();
-      const fetchedbooks = data.items.map((book) => {
-        return { ...book.volumeInfo, id: Math.random().toString(), };
-      });
-      console.log(fetchedbooks);
-      setbooks(fetchedbooks);
-    } catch (error) {
-      alert("something went wrong!")
-    }
-    setIsLoading(false);
-    setbtn(false)
-  };
-  const addBookHandler = (newbook) => {
-    console.log(newbook);
-    setbooks([...books, newbook]);
-    console.log(books);
-  };
-  const bookList = books.map((item) =>
-    <Card key={item.id} iamge={item.imageLinks.smallThumbnail} title={item.title} />
-  );
-  const loadingList = <h1 className='loading' >loading...</h1>
-  const btn = <button className='btn' onClick={getbooks}>Get books</button> */
   return (
     <>
       <Main />
@@ -50,16 +14,6 @@ function App() {
         <Route path="/Home" element={< Home />} />
         <Route path="/About" element={< About />} />
       </Routes>
-      {/* <div className="header">
-        <FindBook />
-        <NewBook onSave={addBookHandler} />
-      </div>
-      {isLoading && loadingList}
-      <div className="container">
-        {!isLoading && bookList}
-
-      </div>
-      {isbtn && btn} */}
     </>
   );
 }
--- src/Components/FindBook.js
+++ src/Components/FindBook.js
@@ -1,31 +1,11 @@
-import { useState } from 'react';
 import './FindBook.css'
-import axios from 'axios';
-import Card from './Card';
-
 function FindBook() {
-   /*  const [search, setsearch] = useState("");
-    const [bookData, setData] = useState([]);
-    const searchBook = (evt) => {
-        if (evt.key === "Enter") {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyCgTzxNyBTZLVvTtCBK6joWCxHaSy9gZQo' + '&maxResults=21')
-                .then(res => setData(res.data.items))
-                .catch(err => console.log(err))
-        }
-    }
-    const bookList = bookData.map((item) =>
-    <Card key={item.id} iamge={item.imageLinks.smallThumbnail} title={item.title} />
-  ); */
 return (
         <>
             <div className="row">
                 <h1>Find Your Book Here</h1>
                 <div className="search">
-                    <input type="text" placeholder="Enter Your Book"
-                        /* value={search}
-                        onChange={e => setsearch(e.target.value)}
-                        onKeyPress={searchBook} */
-                    />
+                    <input type="text" placeholder="Enter Your Book"/>
                     <button ><i className="fa-solid fa-magnifying-glass"></i></button>
                 </div>
             </div>
